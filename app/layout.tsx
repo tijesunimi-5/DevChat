c5e8bfbc6389c4b21cb6408944f51800a4e2444e
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2025-04-30 09:58:37 +0100

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import ClientProviders from "@/components/ClientProviders";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         {/* <MobileBar /> */}
         <ClientProviders>
           {children}
+          <Analytics />
         </ClientProviders>
       </body>
     </html>
